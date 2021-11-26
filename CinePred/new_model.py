33c67b2e4292a1bb120992f0ad7d62cea4789108
PierreLouis98 <pierre-louis.de-soultrait@edu.ece.fr>
2021-11-26 17:30:05 +0100

    Split df in 2, new baseline

--- CinePred/new_model.py
+++ CinePred/new_model.py
@@ -18,8 +18,7 @@ def preproc(df):
     df = keep_columns(df,
                       column_names=[
                           'year', 'date_published', 'genre', 'duration',
-                          'production_company', 'budget',
-                          'worlwide_gross_income'
+                          'budget', 'worlwide_gross_income'
                       ])
     df = remove_na_rows(df)
 
@@ -37,7 +36,7 @@ def preproc(df):
 
     # budget
     df['budget'] = convert_budget_column(df[['budget']])
-    df['budget'] = log_transformation(df[['budget']])
+    #df['budget'] = log_transformation(df[['budget']])
 
     # income
     df['worlwide_gross_income'] = convert_income(df[['worlwide_gross_income']])
@@ -49,13 +48,9 @@ def preproc(df):
 def get_mae(df):
     X = df.drop(columns='worlwide_gross_income')
     y = ['worlwide_gross_income']
-    cv = cross_val_score(XGBRegressor(),
-                         X,
-                         y,
-                         scoring='neg_mean_absolute_error',
-                         cv=5)
-    print(cv)
-    return cv
+    return cross_val_score(XGBRegressor(), X, y,
+                         scoring='neg_mean_absolute_error', cv=5)
+
 if __name__ == '__main__':
     # DECLARE X & Y
     df = import_data('raw_data/IMDb movies.csv')
@@ -63,15 +58,14 @@ if __name__ == '__main__':
     df = preproc(df)
     ohe.fit(df)
     df = ohe.transform(df)
-    print(df.columns)
-    print(df.shape)
+
     df.sort_values('budget', inplace=True)
-    print(type(df.shape[0] / 2))
+    df.reset_index(inplace=True)
+    df.drop(columns='index', inplace=True)
+
     mid = int(df.shape[0] / 2)
-    print(mid)
-    df1 = df['budget'].iloc[:mid].copy()
-    df2 = df['budget'].iloc[mid:].copy()
-    print(df1.shape)
-    print(df2.shape)
-    #get_mae(df1)
-    #get_mae(df2)
+    df1 = df.iloc[:mid]
+    df2 = df.iloc[mid:]
+
+    print(get_mae(df1))
+    prin(get_mae(df2))
