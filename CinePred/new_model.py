bf6df3e3c2735660d275f2808ef2086970f86ab3
PierreLouis98 <pierre-louis.de-soultrait@edu.ece.fr>
2021-11-29 10:35:46 +0100

    GridSearch function, XGBoost params updated

--- CinePred/new_model.py
+++ CinePred/new_model.py
@@ -1,20 +1,21 @@
+from sklearn.pipeline import _name_estimators
 from CinePred.data.importing import *
 from CinePred.data.preprocessing import *
 from CinePred.data.featuring import *
-from CinePred.data.transformers import *
+from CinePred.data.transformers import GenreOHE
 
-from sklearn.model_selection import TimeSeriesSplit, GridSearchCV, cross_validate, cross_val_score
-from sklearn.metrics import mean_absolute_error, r2_score
-from sklearn.pipeline import make_pipeline
-from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import FunctionTransformer, RobustScaler, OneHotEncoder
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.linear_model import LinearRegression
-
-from currency_converter import CurrencyConverter
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from xgboost import XGBRegressor
+import numpy as np
 
 def preproc(df):
+    '''
+        Clean the dataframe
+
+        Input: dataframe
+        Output: dataframe cleaned and sorted by budget
+    '''
+    # NA & columns:
     df = keep_columns(df,
                       column_names=[
                           'year', 'date_published', 'genre', 'duration',
@@ -34,39 +35,73 @@ def preproc(df):
     df['year'] = convert_to_int(df[['year']])
     df['duration'] = convert_to_int(df[['duration']])
 
+    # genre
+    ohe = GenreOHE()
+    ohe.fit(df) # la colonne 'genre' est spécifié dans la classe
+    df = ohe.transform(df)
+
     # budget
     df['budget'] = convert_budget_column(df[['budget']])
-    #df['budget'] = log_transformation(df[['budget']])
+    df['budget'] = log_transformation(df[['budget']])
 
     # income
     df['worlwide_gross_income'] = convert_income(df[['worlwide_gross_income']])
     df['worlwide_gross_income'] = log_transformation(
         df[['worlwide_gross_income']])
 
-    return df
-
-def get_mae(df):
-    X = df.drop(columns=['worlwide_gross_income'])
-    y = df['worlwide_gross_income']
-    model = XGBRegressor()
-    return cross_val_score(model, X, y, cv=5)
-
-
-if __name__ == '__main__':
-    # DECLARE X & Y
-    df = import_data('raw_data/IMDb movies.csv')
-    ohe = GenreOHE()
-    df = preproc(df)
-    ohe.fit(df)
-    df = ohe.transform(df)
-
+    # sort & index:
     df.sort_values('budget', inplace=True)
     df.reset_index(inplace=True)
     df.drop(columns='index', inplace=True)
 
+    return df
+
+
+def get_best_params(model, X, y):
+    # Inspect all pipe components parameters to find the one you want to gridsearch
+    # Instanciate grid search
+    grid_search = GridSearchCV(
+        model,
+        param_grid={
+            # Access any component of the pipeline, as far back as you want
+            'learning_rate': [0.01, 0.1],
+            'n_estimators': [100, 200, 300],
+            'max_depth': [2]
+        },
+        cv=5,
+        scoring="neg_mean_absolute_error",
+        verbose=2)
+
+    grid_search.fit(X, y)
+    return grid_search.best_params_
+
+
+def get_mae(df):
+    X = df.drop(columns=['worlwide_gross_income'])
+    y = df['worlwide_gross_income']
+    model = XGBRegressor(learning_rate=0.1, max_depth=2)
+    
+    return cross_val_score(model, X, y, cv=5, scoring='neg_mean_absolute_error')
+
+def predict(df):
+    '''
+        Input: a preprocessed df
+        Output: 2 scores MAE scores '''
     mid = int(df.shape[0] / 2)
     df1 = df.iloc[:mid].copy()
     df2 = df.iloc[mid:].copy()
 
-    print(get_mae(df1))
-    print(get_mae(df2))
+    score1 = np.mean(np.abs(get_mae(df1))) # {'learning_rate': 0.1, 'max_depth': 2, 'n_estimators': 200}
+    score2 = np.mean(np.abs(get_mae(df2))) # {'learning_rate': 0.1, 'max_depth': 2, 'n_estimators': 300}
+    #return [score1, score2]
+    return [round(score1, 2), round(score2, 2)]
+
+if __name__ == '__main__':
+    # Import
+    df = import_data('raw_data/IMDb movies.csv')
+
+    # Prepare
+    df = preproc(df)
+
+    # Predict
+    print(predict(df))
