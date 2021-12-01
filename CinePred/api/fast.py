554d27cbde321a324f915dfb60b1540797cae6c0
Oscar Touze <oscartouze@hotmail.fr>
2021-12-01 14:40:52 +0100

    dockers

--- CinePred/api/fast.py
+++ CinePred/api/fast.py
@@ -38,13 +38,14 @@ def search_movie(title):
     movie_dic = {}
     for i in range(7000,8000):
         movie_dic[df_preproc['title'].iloc[0:df_preproc.shape[0]][i]] = {'Actors' : df_preproc['actors'].iloc[0:df_preproc.shape[0]][i],
+                                                        #  'Year' : df_preproc['year'].iloc[0:df_preproc.shape[0]][i],
                                                          'Country' : df_preproc['country'].iloc[0:df_preproc.shape[0]][i],
                                                          'Income' : np.round(10**(df_preproc['worlwide_gross_income'].iloc[0:df_preproc.shape[0]][i]),2),
                                                          'Budget' : np.round(10**(df_preproc['budget'].iloc[0:df_preproc.shape[0]][i]),2),
-                                                        #  'Numbers of blockbuster' : df_preproc['shifted'].iloc[0:df.shape[0]][i],
+                                                        # #  'Numbers of blockbuster' : df_preproc['shifted'].iloc[0:df.shape[0]][i],
                                                          'Description' : df_preproc['description'].iloc[0:df.shape[0]][i],
                                                          'Avg_vote' : df_preproc['avg_vote'].iloc[0:df.shape[0]][i],
-                                                        #  'Duration' : df_preproc['duration'].iloc[0:df.shape[0]][i],
+                                                        # #  'Duration' : df_preproc['duration'].iloc[0:df.shape[0]][i],
                                                          'Production company' : df_preproc['production_company'].iloc[0:df.shape[0]][i] ,
                                                          'Director' : df_preproc['director'].iloc[0:df.shape[0]][i]
                                                          }
@@ -60,14 +61,14 @@ def movies():
     for i in range(7000,8000):
 
         movie_dic[df_preproc['title'].iloc[0:df_preproc.shape[0]][i]] = {'Actors' : df_preproc['actors'].iloc[0:df_preproc.shape[0]][i],
-                                                         'Year' : df_preproc['year'].iloc[0:df_preproc.shape[0]][i],
+                                                        #  'Year' : df_preproc['year'].iloc[0:df_preproc.shape[0]][i],
                                                          'Country' : df_preproc['country'].iloc[0:df_preproc.shape[0]][i],
                                                          'Income' : np.round(10**(df_preproc['worlwide_gross_income'].iloc[0:df_preproc.shape[0]][i]),2),
                                                          'Budget' : np.round(10**(df_preproc['budget'].iloc[0:df_preproc.shape[0]][i]),2),
-                                                        #  'Numbers of blockbuster' : df_preproc['shifted'].iloc[0:df.shape[0]][i],
+                                                        # #  'Numbers of blockbuster' : df_preproc['shifted'].iloc[0:df.shape[0]][i],
                                                          'Description' : df_preproc['description'].iloc[0:df.shape[0]][i],
                                                          'Avg_vote' : df_preproc['avg_vote'].iloc[0:df.shape[0]][i],
-                                                        #  'Duration' : df_preproc['duration'].iloc[0:df.shape[0]][i],
+                                                        # #  'Duration' : df_preproc['duration'].iloc[0:df.shape[0]][i],
                                                          'Production company' : df_preproc['production_company'].iloc[0:df.shape[0]][i] ,
                                                          'Director' : df_preproc['director'].iloc[0:df.shape[0]][i]
                                                          }
@@ -78,8 +79,7 @@ def movies():
 
 @app.get("/search_actor")
 def search_actors(name):
-    movie_list = df_preproc[df_preproc
-                            ['actors'].str.contains(name)]['title'].values.tolist()
+    movie_list = df_preproc[df_preproc['actors'].str.contains(name)]['title'].values.tolist()
     actor_dict =  {}
     list_index = np.arange(0,len(movie_list)).tolist()
 
@@ -127,23 +127,23 @@ def test( director='Steven Spielberg',
 
 
     #----   Init Dataframe ----#
-    df = pd.DataFrame({'year': [year]})
-    df['year'] = year
-    df['duration'] = duration
-    df['budget'] = budget
-    df['genre'] = genre
-    df['director'] = director
-    df['writer'] = writer
-    df['production_company'] = production_company
-    df['actors'] = f'{main_actor}, {second_actor}, {third_actor}'
-    df['date_published'] = date_published
+    df_sandbox = pd.DataFrame({'year': [year]})
+    df_sandbox['year'] = year
+    df_sandbox['duration'] = duration
+    df_sandbox['budget'] = budget
+    df_sandbox['genre'] = genre
+    df_sandbox['director'] = director
+    df_sandbox['writer'] = writer
+    df_sandbox['production_company'] = production_company
+    df_sandbox['actors'] = f'{main_actor}, {second_actor}, {third_actor}'
+    df_sandbox['date_published'] = date_published
 
     #----   preproc   ----#
-    df['year'] = convert_to_int(df[['year']])
-    df['duration'] = convert_to_int(df[['duration']])
-    df['budget'] = convert_to_int(df[['budget']]) * 1_000_000
+    df_sandbox['year'] = convert_to_int(df_sandbox[['year']])
+    df_sandbox['duration'] = convert_to_int(df_sandbox[['duration']])
+    df_sandbox['budget'] = convert_to_int(df_sandbox[['budget']]) * 1_000_000
 
-    df['budget'] = log_transformation(df[['budget']])
+    df_sandbox['budget'] = log_transformation(df_sandbox[['budget']])
 
     actors_1 = df_preproc[['shifted', 'actors']][df_preproc[[
         'shifted', 'actors'
@@ -155,39 +155,39 @@ def test( director='Steven Spielberg',
         'shifted', 'actors'
     ]]['actors'].str.contains(third_actor)].max()['shifted']
     shifted = actors_1 + actors_2 + actors_3
-    df['shifted'] = shifted
+    df_sandbox['shifted'] = shifted
 
-    df['date_published'] = convert_to_date(df[['date_published']])
-    df['date_sin'] = add_sin_features(df[['date_published']])
-    df['date_cos'] = add_cos_features(df[['date_published']])
-    df.drop(columns='date_published', inplace=True)
+    df_sandbox['date_published'] = convert_to_date(df_sandbox[['date_published']])
+    df_sandbox['date_sin'] = add_sin_features(df_sandbox[['date_published']])
+    df_sandbox['date_cos'] = add_cos_features(df_sandbox[['date_published']])
+    df_sandbox.drop(columns='date_published', inplace=True)
 
     ohe = GenreOHE()
-    ohe.fit(df)  # la colonne 'genre' est spécifié dans la classe
-    df = ohe.transform(df)
+    ohe.fit(df_sandbox)  # la colonne 'genre' est spécifié dans la classe
+    df_ohe = ohe.transform(df_sandbox)
 
-    df['Nb_actuals_movie_directors_company'] = df_preproc[[
+    df_ohe['Nb_actuals_movie_directors_company'] = df_preproc[[
         'Nb_actuals_movie_directors_company', 'production_company'
     ]][df_preproc[[
         'Nb_actuals_movie_directors_company', 'production_company'
     ]]['production_company'].str.contains(
         production_company)].max()['Nb_actuals_movie_directors_company']
-    df['Nb_actuals_movie_directors'] = df_preproc[[
+    df_ohe['Nb_actuals_movie_directors'] = df_preproc[[
         'Nb_actuals_movie_directors', 'director'
     ]][df_preproc[[
         'Nb_actuals_movie_directors', 'director'
     ]]['director'].str.contains(director)].max()['Nb_actuals_movie_directors']
-    df['Nb_actuals_movie_directors_writer'] = df_preproc[[
+    df_ohe['Nb_actuals_movie_directors_writer'] = df_preproc[[
         'Nb_actuals_movie_directors_writer', 'writer'
     ]][df_preproc[['Nb_actuals_movie_directors_writer',
                    'writer']]['writer'].str.contains(
                        writer)].max()['Nb_actuals_movie_directors_writer']
 
-    df = df.drop(columns=['actors'])
+    df_drop = df_ohe.drop(columns=['actors'])
 
-    df = df.drop(columns=['production_company', 'director', 'writer'])
+    df_drop2 = df_drop.drop(columns=['production_company', 'director', 'writer'])
     #----   Prediction   ----#
     model = load_model("model.joblib")
-    result = predict_fromX(model, df)
+    result = predict_fromX(model, df_drop2)
 
     return dict(income = result)
